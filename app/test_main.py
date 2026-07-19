7a0d4396d482d53a96d11e38573bf2d1c6cd771b
Andrii Zelenskyi <Zelenskiy2013@gmail.com>
2026-07-19 22:35:39 +0300

    fix according mentor's comments v.1

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from app.main import can_access_google_page
+import app.main
 
 
 @mock.patch("app.main.has_internet_connection")
@@ -11,41 +11,29 @@ def test_can_access_google_page_when_url_valid_and_has_internet(
 ) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = True
-    result = can_access_google_page("https://google.com")
-    assert result is True
+    result = app.main.can_access_google_page("https://google.com")
+    assert result == "Accessible"
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_when_url_invalid(
+def test_cannot_access_when_only_connection_exists(
     mock_valid_google_url: mock.MagicMock,
     mock_has_internet_connection: mock.MagicMock,
 ) -> None:
     mock_valid_google_url.return_value = False
     mock_has_internet_connection.return_value = True
-    result = can_access_google_page("https://youtube.com")
+    result = app.main.can_access_google_page("https://youtube.com")
     assert result == "Not accessible"
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_when_connection_is_lost(
+def test_cannot_access_when_only_url_is_valid(
     mock_valid_google_url: mock.MagicMock,
     mock_has_internet_connection: mock.MagicMock,
 ) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = False
-    result = can_access_google_page("https://youtube.com")
+    result = app.main.can_access_google_page("https://google.com")
     assert result == "Not accessible"
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_when_all_data_is_valid(
-    mock_valid_google_url: mock.MagicMock,
-    mock_has_internet_connection: mock.MagicMock,
-) -> None:
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = True
-    result = can_access_google_page("https://youtube.com")
-    assert result == "Accessible"
